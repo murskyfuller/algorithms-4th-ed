bad573dcb258c147080cf12883c0949cf862af4f
Jesse <murskyfuller@gmail.com>
2020-12-17 17:24:07 -0500

    Function annotations

--- Chapter 1/js/1.1.3.js
+++ Chapter 1/js/1.1.3.js
@@ -1,7 +1,16 @@
+/**
+ * Shallow comparison of all arguments.
+ * @param  {...any} args The values to check.
+ * @returns True if all arguments are equal to each other, otherwise false.
+ */
 function areEqual(...args) {
   throw new Error('Not implemented.')
 }
 
+/**
+ * Prompts user for integer input via the console and prints
+ * either 'equal' or 'not equal' depending on the input.
+ */
 function solution() {
   throw new Error('Not implemented.')
 }
